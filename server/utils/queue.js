a44abd584d0a6b584d1967fbd8d9033c0dde1007
ticnap-notnac <ronaldpacioles9@gmail.com>
2026-07-06 20:04:58 +0800

    fix: correct pg-boss ESM named export in queue initialization

--- server/utils/queue.js
+++ server/utils/queue.js
@@ -1,6 +1,4 @@
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-const PgBoss = require('pg-boss')
+import { PgBoss } from 'pg-boss'
 import logger from './logger.js'
 
 if (!process.env.DATABASE_URL) {
